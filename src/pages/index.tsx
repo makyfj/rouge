086ec03ff75be4730fad25fda3e74a1cf49c4d17
Franklin Jara <franklinjaradev@gmail.com>
2022-08-26 00:52:29 -0400

    :busts_in_silhouette: Styling for sign in/out

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
       </Head>
       <div className="container mx-auto p-4">
         <div className="flex h-screen">
-          <div className="m-auto">
+          <div className="m-auto flex flex-col gap-4">
             <h1 className="text-center font-bold text-4xl">Rouge</h1>
             <div className="grid grid-cols-2 gap-4 place-items-center">
               <div className="text-3xl">
@@ -29,18 +29,27 @@ const Home: NextPage = () => {
                 ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
                 Elit sunt amet fugiat veniam occaecat fugiat aliqua.
               </div>
-              <div className="bg-cyan-400 rounded p-4 flex flex-col gap-4">
+              <div className="bg-gradient-to-br from-cyan-500 to-cyan-600 rounded p-4 flex flex-col gap-4 shadow-md shadow-cyan-800">
                 {session ? (
                   <>
                     <p className="text-xl text-center font-bold">
-                      Signed in as {session?.user?.email}
+                      Signed in as {" "}
+                      <span className="text-cyan-50">
+                        {session?.user?.name}
+                      </span>
                     </p>
                     <div className="flex gap-4 justify-center">
                       <button
                         onClick={() => signOut()}
-                        className="p-4 border border-black rounded"
+                        className="p-4 border border-black rounded text-cyan-50"
                       >
-                        <FiLogOut className="text-5xl" />
+                        Workout
+                      </button>
+                      <button
+                        onClick={() => signOut()}
+                        className="p-4 border border-black rounded text-cyan-50"
+                      >
+                        Logout
                       </button>
                     </div>
                   </>
