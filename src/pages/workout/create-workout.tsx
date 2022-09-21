2f1c0e016a5559639c608a223da151e2911dfc4a
Franklin Jara <franklinjaradev@gmail.com>
2022-09-20 23:49:53 -0400

    :zap: chore: adding mutations and queries for workout page

--- src/pages/workout/create-workout.tsx
+++ src/pages/workout/create-workout.tsx
@@ -1,6 +1,20 @@
+import {useSession} from 'next-auth/react'
+import {useEffect} from 'react'
+import {useRouter} from 'next/router'
+
 import Menu from 'src/components/menu'
+import {trpc} from 'src/utils/trpc'
 
 const CreateWorkout = () => {
+	const createWorkout = trpc.workout.createWorkout.useMutation()
+	const router = useRouter()
+	const {data: session} = useSession()
+
+	useEffect(() => {
+		if (!session) {
+			router.push('/')
+		}
+	}, [router, session])
 	return (
 		<Menu>
 			<h1 className='pages-title'>Create Workout</h1>
--- src/pages/workout/view-workouts.tsx
+++ src/pages/workout/view-workouts.tsx
@@ -1,6 +1,20 @@
+import {useSession} from 'next-auth/react'
+import {useEffect} from 'react'
+import {useRouter} from 'next/router'
+
 import Menu from 'src/components/menu'
+import {trpc} from 'src/utils/trpc'
 
 const ViewWorkouts = () => {
+	const router = useRouter()
+	const {data: session} = useSession()
+	const viewWorkouts = trpc.workout.getWorkouts.useQuery()
+
+	useEffect(() => {
+		if (!session) {
+			router.push('/')
+		}
+	}, [router, session])
 	return (
 		<Menu>
 			<h1 className='pages-title'>View Workouts</h1>
